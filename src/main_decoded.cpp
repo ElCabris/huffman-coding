8f1ed615c70eb103e098ab26fd31470b8b67d2d5
Emanuel Cabrera Novoa <emanuelcabreranovoa@hotmail.com>
2024-11-07 15:10:29 -0500

    feat: add command line arguments

--- src/main_decoded.cpp
+++ src/main_decoded.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <tree.hpp>
 
-int main() {
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    std::cerr << "invalid arguments" << std::endl;
+    return -1;
+  }
   auto root = huffman::load_huffman_tree_from_json("tree.json");
   huffman::print_huffman_tree(root);
 
   std::cout << std::endl;
 
-  huffman::decode_file("encode.txt", "decoded.txt", root);
+  huffman::decode_file(argv[1], argv[2], root);
 
   huffman::delete_huffman_tree(root);
 }
--- src/main_encoded.cpp
+++ src/main_encoded.cpp
@@ -1,16 +1,21 @@
 #include <files.hpp>
+#include <iostream>
 #include <tree.hpp>
 
-int main() {
-  auto frequencies = huffman::get_character_frequencies("text.txt");
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    std::cerr << "invalid arguments" << std::endl;
+    return -1;
+  }
+  auto frequencies = huffman::get_character_frequencies(argv[1]);
 
   auto root = huffman::build_huffman_tree(frequencies);
 
-  huffman::print_huffman_tree(root);
+  huffman::encode_file(argv[1], argv[2], root);
 
   huffman::write_huffman_tree_to_json(root, "tree.json");
 
-  huffman::encode_file("text.txt", "encode.txt", root);
+  huffman::print_huffman_tree(root);
 
   huffman::delete_huffman_tree(root);
 
